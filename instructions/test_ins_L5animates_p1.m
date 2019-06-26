2288828756c283882a992bb2811a7980ded8c2f0
hyang336 <haozi078@gmail.com>
2019-06-26 14:01:19 -0400

    changed the lifetime example to something more everyday.

--- instructions/test_ins_L5animates_p1.m
+++ instructions/test_ins_L5animates_p1.m
@@ -6,10 +6,9 @@
 %the least frequent. For new words, your task is to judge
 %how familiar you are with the thing that the word
 %represents based on your lifetime experience. For instance,
-%a person who majored in neuroscience during his university
-%years may have a lot of experience with "brain" but little
-%experience with "stock", while a person who majored in
-%business may say the opposite. The two tasks will
-%alternate in five-trial blocks. At the beginning of each
-%block, a message will inform you what the next five trials
-%will be.[press your right index finger to go to next page]
+%most canadians would say they are more familiar with
+%"apple" than "mango". But if you are a mango-enthusiast,
+%you may say the opposite. The two tasks will alternate in
+%five-trial blocks. At the beginning of each block, a
+%message will inform you what the next five trials will
+%be.[press your right index finger to go to next page]
