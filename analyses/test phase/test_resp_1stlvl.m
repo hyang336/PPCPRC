e4845ae3e4e6f69dee73eab183709f3167c9ac86
hyang336 <haozi078@gmail.com>
2019-10-23 15:59:52 -0400

    finished 1st lvl analysis script for test phase resp. TODO: add eye-tracking data into the design matrix; add an option to replace noresp trials with normative data.

--- analyses/test phase/test_resp_1stlvl.m
+++ analyses/test phase/test_resp_1stlvl.m
@@ -7,7 +7,7 @@
 %with norm_fam and obj_freq "replace")
 
 
-function test_1stlvl(project_derivative,output,sub,expstart_vol,fmriprep_foldername,TR,noresp_opt)
+function test_resp_1stlvl(project_derivative,output,sub,expstart_vol,fmriprep_foldername,TR,noresp_opt)
 %(i.e. if there are 4 dummy scans, the experiment starts at the 5th
 %TR/trigger/volume). In this version every participant in every run has to have the same number of
 %dummy scans. 
@@ -36,9 +36,13 @@ sub_dir=strcat(output,'/test_1stlvl/',sub);
             %get #runs from each /fmriprep/subject/func/ folder, search for task
             %"lifetime" for pilot subjects
             
-            % 20190929 remember to change the task name to
-            % "test" for actual participants
-            runkey=fullfile(strcat(project_derivative,'/',fmriprep_foldername,'/fmriprep/',sub,'/func/'),'*lifetime*_space-MNI152NLin2009cAsym*preproc*.nii.gz');
+            %% 20190929 change the task name to
+            %% "test" for participants 002 and onward
+            if strcmp(sub,'sub-001')
+                runkey=fullfile(strcat(project_derivative,'/',fmriprep_foldername,'/fmriprep/',sub,'/func/'),'*lifetime*_space-MNI152NLin2009cAsym*preproc*.nii.gz');
+            else
+                runkey=fullfile(strcat(project_derivative,'/',fmriprep_foldername,'/fmriprep/',sub,'/func/'),'*test*_space-MNI152NLin2009cAsym*preproc*.nii.gz');
+            end
             runfile=dir(runkey);
             substr=struct();
             substr.run=extractfield(runfile,'name');
@@ -81,14 +85,15 @@ switch noresp_opt
 %                 mkdir(temp_dir,strcat(task{1},erase(run{1},'_')));
 %                 run_temp=strcat(temp_dir,strcat(task{1},erase(run{1},'_')));
               
-                %change these to what types of block you have
+                %change these to what types of block you
+                %have, the column numbers are *hard-coded*
                 freq_trials=substr.runevent{j}(strcmp(substr.runevent{j}(:,4),'recent'),:);
                 fam_trials=substr.runevent{j}(strcmp(substr.runevent{j}(:,4),'lifetime'),:);
-                recent_1=freq_trials(cellfun(@(x)x==1,fam_trials(:,6)),:);
-                recent_2=freq_trials(cellfun(@(x)x==2,fam_trials(:,6)),:);
-                recent_3=freq_trials(cellfun(@(x)x==3,fam_trials(:,6)),:);
-                recent_4=freq_trials(cellfun(@(x)x==4,fam_trials(:,6)),:);
-                recent_5=freq_trials(cellfun(@(x)x==5,fam_trials(:,6)),:);
+                recent_1=freq_trials(cellfun(@(x)x==1,freq_trials(:,6)),:);
+                recent_2=freq_trials(cellfun(@(x)x==2,freq_trials(:,6)),:);
+                recent_3=freq_trials(cellfun(@(x)x==3,freq_trials(:,6)),:);
+                recent_4=freq_trials(cellfun(@(x)x==4,freq_trials(:,6)),:);
+                recent_5=freq_trials(cellfun(@(x)x==5,freq_trials(:,6)),:);
                 lifetime_1=fam_trials(cellfun(@(x)x==1,fam_trials(:,6)),:);
                 lifetime_2=fam_trials(cellfun(@(x)x==2,fam_trials(:,6)),:);
                 lifetime_3=fam_trials(cellfun(@(x)x==3,fam_trials(:,6)),:);
@@ -109,10 +114,10 @@ switch noresp_opt
                 comma=repmat(',',(length(substr.runexp{j})-expstart_vol+1),1);
                 comma=cellstr(comma);
                 prefix=cell(length(slice),1);
-                prefix(:)={substr.runsmooth{j}};%should be a unique run name
+                prefix(:)={substr.runsmooth{j}};%should be a unique run name (using smoothed data)
                 %prefix=prefix';
                 sliceinfo=cellfun(@strcat,prefix,comma,slice,'UniformOutput',false);
-                %% 2019-10-02 
+                
                 %an indicator for which condition is missing in a given run
                 cond={'recent_1','recent_2','recent_3','recent_4','recent_5','lifetime_1','lifetime_2','lifetime_3','lifetime_4','lifetime_5','noresp';recent_1,recent_2,recent_3,recent_4,recent_5,lifetime_1,lifetime_2,lifetime_3,lifetime_4,lifetime_5,noresp};
                 [~,have_cond]=find(cellfun(@(x)~isempty(x),cond(2,:)));
@@ -128,10 +133,16 @@ switch noresp_opt
                 
                 matlabbatch{1}.spm.stats.fmri_spec.sess(j).scans = sliceinfo;
                 
-                for k=1:length(have_cond)
-                matlabbatch{1}.spm.stats.fmri_spec.sess(j).cond(k).name = cond{1,have_cond(k)};
-                matlabbatch{1}.spm.stats.fmri_spec.sess(j).cond(k).onset = cell2mat(cond{2,have_cond(k)}(:,1));
-                matlabbatch{1}.spm.stats.fmri_spec.sess(j).cond(k).duration = cell2mat(cond{2,have_cond(k)}(:,2));
+                %loop through conitions in a run to fill the
+                %matlabbatch structure
+                for k=1:length(have_cond)%again the column number here is *hard-coded*
+                    matlabbatch{1}.spm.stats.fmri_spec.sess(j).cond(k).name = cond{1,have_cond(k)};
+                    matlabbatch{1}.spm.stats.fmri_spec.sess(j).cond(k).onset = cell2mat(cond{2,have_cond(k)}(:,1));
+                    matlabbatch{1}.spm.stats.fmri_spec.sess(j).cond(k).duration = cell2mat(cond{2,have_cond(k)}(:,5));
+                    %gotta fill these fields too
+                    matlabbatch{1}.spm.stats.fmri_spec.sess(j).cond(k).tmod = 0;
+                    matlabbatch{1}.spm.stats.fmri_spec.sess(j).cond(k).pmod = struct('name', {}, 'param', {}, 'poly', {});
+                    matlabbatch{1}.spm.stats.fmri_spec.sess(j).cond(k).orth = 1;
                 end
                 %always have 6 motion regressors
                 matlabbatch{1}.spm.stats.fmri_spec.sess(j).regress(1).name = 'x_move';
@@ -146,49 +157,107 @@ switch noresp_opt
                 matlabbatch{1}.spm.stats.fmri_spec.sess(j).regress(5).val = substr.runconf{j}.rot_y(expstart_vol:end);
                 matlabbatch{1}.spm.stats.fmri_spec.sess(j).regress(6).name = 'z_rot';
                 matlabbatch{1}.spm.stats.fmri_spec.sess(j).regress(6).val = substr.runconf{j}.rot_z(expstart_vol:end);
-                               
-                
+                        
             end
+                
                 %specify run-agnostic fields
                 matlabbatch{1}.spm.stats.fmri_spec.dir = {temp_dir};%all runs are combined into one
                 matlabbatch{1}.spm.stats.fmri_spec.timing.units = 'secs';
                 matlabbatch{1}.spm.stats.fmri_spec.timing.RT = TR;%remember to change this according to actual TR in second
-                matlabbatch{2}.spm.stats.fmri_est.spmmat = {strcat(temp_dir,'/SPM.mat')};
-                matlabbatch{3}.spm.stats.con.spmmat = {strcat(temp_dir,'/SPM.mat')};
+                %estimate the specified lvl-1 model
+                matlabbatch{2}.spm.stats.fmri_est.spmmat = {strcat(temp_dir,'SPM.mat')};
+                
+                %% setup the two contrast, lifetime and recent. Note that "as long as all of the contrasts are derived from the same GLM model, then you can have as many as you want in a single SPM.mat" ---Suzanne Witt
+                %setup linear contrast for lifetime
+                %conditions
+                matlabbatch{3}.spm.stats.con.spmmat = {strcat(temp_dir,'SPM.mat')};
                 matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'linear inc lifetime';
                 %use runbycond to construct appropriate contrast vector
-                %linear increase lifetime
+                %lifetime conditions
                 [lifetime1_runrow,lifetime1_designcol]=find(cellfun(@(x)strcmp(x,'lifetime_1'),runbycond));
                 [lifetime2_runrow,lifetime2_designcol]=find(cellfun(@(x)strcmp(x,'lifetime_2'),runbycond));
                 [lifetime3_runrow,lifetime3_designcol]=find(cellfun(@(x)strcmp(x,'lifetime_3'),runbycond));
                 [lifetime4_runrow,lifetime4_designcol]=find(cellfun(@(x)strcmp(x,'lifetime_4'),runbycond));
                 [lifetime5_runrow,lifetime5_designcol]=find(cellfun(@(x)strcmp(x,'lifetime_5'),runbycond));
+                %need to pull recent conditions as well to
+                %give them 0 weights instead of NaN
+                [recent1_runrow,recent1_designcol]=find(cellfun(@(x)strcmp(x,'recent_1'),runbycond));
+                [recent2_runrow,recent2_designcol]=find(cellfun(@(x)strcmp(x,'recent_2'),runbycond));
+                [recent3_runrow,recent3_designcol]=find(cellfun(@(x)strcmp(x,'recent_3'),runbycond));
+                [recent4_runrow,recent4_designcol]=find(cellfun(@(x)strcmp(x,'recent_4'),runbycond));
+                [recent5_runrow,recent5_designcol]=find(cellfun(@(x)strcmp(x,'recent_5'),runbycond));
+                
                 [noresp_runrow,noresp_designcol]=find(cellfun(@(x)strcmp(x,'noresp'),runbycond));%although not included in the contrast, need to account for all conditions that may differ between runs since they affect column numbers in the design matrix
                 motionzero=zeros(size(runbycond,1),6);%6 motion regressors
+                
                 conmat=nan(size(runbycond));%can't use 0 since noresp is given 0 weight
+                %lifetime condition index
                 l1ind=sub2ind(size(conmat),lifetime1_runrow,lifetime1_designcol);
                 l2ind=sub2ind(size(conmat),lifetime2_runrow,lifetime2_designcol);
                 l3ind=sub2ind(size(conmat),lifetime3_runrow,lifetime3_designcol);
                 l4ind=sub2ind(size(conmat),lifetime4_runrow,lifetime4_designcol);
                 l5ind=sub2ind(size(conmat),lifetime5_runrow,lifetime5_designcol);
+                %recent condition index
+                r1ind=sub2ind(size(conmat),recent1_runrow,recent1_designcol);
+                r2ind=sub2ind(size(conmat),recent2_runrow,recent2_designcol);
+                r3ind=sub2ind(size(conmat),recent3_runrow,recent3_designcol);
+                r4ind=sub2ind(size(conmat),recent4_runrow,recent4_designcol);
+                r5ind=sub2ind(size(conmat),recent5_runrow,recent5_designcol);
+                %noresp index
                 nrind=sub2ind(size(conmat),noresp_runrow,noresp_designcol);
+                %linear increase lifetime
                 conmat(l1ind)=-2/length(lifetime1_runrow);
                 conmat(l2ind)=-1/length(lifetime2_runrow);
                 conmat(l3ind)=0;
                 conmat(l4ind)=1/length(lifetime4_runrow);
                 conmat(l5ind)=2/length(lifetime5_runrow);
-                conmat(nrind)=0;
                 
+                conmat(r1ind)=0;
+                conmat(r2ind)=0;
+                conmat(r3ind)=0;
+                conmat(r4ind)=0;
+                conmat(r5ind)=0;
+                conmat(nrind)=0;
+                                
                 conmat=[conmat motionzero];%attach motion regressor weights (i.e. 0)
-                convec=reshape(conmat',1,48);%unroll the matrix into a vector
+                convec=reshape(conmat',1,numel(conmat));%unroll the matrix into a vector
                 convec=convec(~isnan(convec));                
                 matlabbatch{3}.spm.stats.con.consess{1}.tcon.weights = convec;
+                                           
+                %% recent linear contrast (put it in a different consess)
+                matlabbatch{3}.spm.stats.con.spmmat = {strcat(temp_dir,'SPM.mat')};
+                matlabbatch{3}.spm.stats.con.consess{2}.tcon.name = 'linear dec recent';
                 
-                matlabbatch{4}.spm.stats.results.spmmat = {strcat(temp_dir,'/SPM.mat')};
-                matlabbatch{4}.spm.stats.results.conspec.titlestr = 'linear inc lifetime fwe';
-                matlabbatch{4}.spm.stats.results.conspec.contrasts = 1;
-                matlabbatch{4}.spm.stats.results.export{2}.tspm.basename = 'linear inc lifetime fwe';%for details about threshold and correction, see localizer_template_job.m
+                conmat=nan(size(runbycond));%can't use 0 since noresp is given 0 weight
+                conmat(r1ind)=2/length(recent1_runrow);
+                conmat(r2ind)=1/length(recent2_runrow);
+                conmat(r3ind)=0;
+                conmat(r4ind)=-1/length(recent4_runrow);
+                conmat(r5ind)=-2/length(recent5_runrow);
+                
+                conmat(l1ind)=0;
+                conmat(l2ind)=0;
+                conmat(l3ind)=0;
+                conmat(l4ind)=0;
+                conmat(l5ind)=0;
+                conmat(nrind)=0;
+                                
+                conmat=[conmat motionzero];%attach motion regressor weights (i.e. 0)
+                convec=reshape(conmat',1,numel(conmat));%unroll the matrix into a vector
+                convec=convec(~isnan(convec));                
+                matlabbatch{3}.spm.stats.con.consess{2}.tcon.weights = convec;
+                
+                %% results (thresholded)
+                matlabbatch{4}.spm.stats.results.spmmat = {strcat(temp_dir,'SPM.mat')};
+                matlabbatch{4}.spm.stats.results.export{2}.tspm.basename = 'test resp fwe';%for details about threshold and correction, see xxx_template_job.m
+                %first contrast
+                matlabbatch{4}.spm.stats.results.conspec(1).titlestr = 'linear inc lifetime fwe';
+                matlabbatch{4}.spm.stats.results.conspec(1).contrasts = 1;               
+                %second contrast
+                matlabbatch{4}.spm.stats.results.conspec(2).titlestr = 'linear dec recent fwe';
+                matlabbatch{4}.spm.stats.results.conspec(2).contrasts = 2;               
             
+                
             %initil setup for SPM
             spm('defaults', 'FMRI');
             spm_jobman('initcfg');
@@ -197,6 +266,7 @@ switch noresp_opt
             spm_jobman('run',matlabbatch);
             
    %% replacing noresp trials with norm_fam or obj_freq
+   %% 2019-10-21 line run in sub-P005_script.mat
     case 'replace'
         
         test_1stlvl_template_job;
