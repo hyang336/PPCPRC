d365dcfdcd7fd4e6b5663d6adb89f0761320bced
hyang336 <hyang336@uwo.ca>
2020-03-09 18:02:43 -0400

    changed the test phase instruction to make it clearer that old words are tied to recent task and new words with lifetime task

--- instructions/test_ins_L5animates_p1.m
+++ instructions/test_ins_L5animates_p1.m
@@ -1,14 +1,14 @@
 %In this part, you will see one word at a time, which can be
 %either an old word (i.e. you have seen it when making
-%animacy judgements) or a new word. For old words, your task
-%is to judge how frequently you have seen each of them on a
-%5-point scale, with 5 indicating the most frequent, and 1
-%the least frequent. For new words, your task is to judge
-%how familiar you are with the thing that the word
-%represents based on your lifetime experience. For instance,
-%most canadians would say they are more familiar with
-%"apple" than "mango". But if you are a mango-enthusiast,
-%you may say the opposite. The two tasks will alternate in
-%five-trial blocks. At the beginning of each block, a
-%message will inform you what the next five trials will
-%be.[press your right index finger to go to next page]
+%animacy judgements) or a new word. For old words (recent
+%task), your task is to judge how frequently you have seen
+%each of them on a 5-point scale, with 5 indicating the most
+%frequent, and 1 the least frequent. For new words (lifetime
+%task), your task is to judge how familiar you are with the
+%thing that the word represents based on your lifetime
+%experience. For instance, most canadians would say they are
+%more familiar with "apple" than "mango". But if you are a
+%mango-enthusiast, you may say the opposite. The two tasks
+%alternate in five-trial blocks. At the beginning of each
+%block, a message will inform you what the next five trials
+%will be.[press right index finger to go to next page]
