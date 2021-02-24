bc8d4105f799a9a0f14a0e37fd56edb23b8302b7
Haopei Yang <hyang336@gra-login2.graham.sharcnet>
2021-02-24 18:42:39 -0500

    now save all tSNR

--- analyses/tsnr_exclusion.m
+++ analyses/tsnr_exclusion.m
@@ -10,10 +10,12 @@ sublist=sublist{1,1};
 
 runlist={'keyprac_run-01','study_run-01','study_run-02','study_run-03','study_run-04','study_run-05','test_run-01','test_run-02','test_run-03','test_run-04'};
 
+tSNR_res=cell(length(sublist)+1,length(runlist)+1);
+tSNR_res(1,2:end)=runlist;
 
 for a=1:length(sublist)
     s=sublist{a};
-
+    tSNR_res{a+1,1}=s;
     for b=1:length(runlist)
         filename = strcat('sub-',s,'_task-',runlist{b},'_space-T1w_desc-preproc_bold.nii.gz');
         runfile=niftiread(strcat(fmriprep_dir,'/fmriprep/sub-',s,'/func/',filename));
@@ -31,7 +33,7 @@ for a=1:length(sublist)
         if PrC_tSNR<10
             tSNR_Bad(a,b)=cellstr(strcat('sub-',s,'_task-',runlist{b}));
         end
-        
+        tSNR_res{a+1,b+1}=PrC_tSNR;
     end
 end
-save(output_dir,'tSNR_Bad');
+%save(output_dir,'tSNR_Bad');
