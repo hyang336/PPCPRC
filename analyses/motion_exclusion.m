f16d4b6f46b101fe91f6ec435222cdc641cb76f8
Haopei Yang <hyang336@gra-login2.graham.sharcnet>
2021-04-12 13:26:15 -0400

    changed scratch directory

--- analyses/motion_exclusion.m
+++ analyses/motion_exclusion.m
@@ -1,7 +1,7 @@
-output_dir='~/elusive_scratch/working_dir/PPC_MD/';
-fmriprep_dir='~/elusive_scratch/working_dir/PPC_MD/fmriprep_1.5.4_corrected';
+output_dir='~/scratch/working_dir/PPC_MD/';
+fmriprep_dir='~/scratch/working_dir/PPC_MD/fmriprep_1.5.4_AROMA';
 
-fid = fopen('~/elusive_scratch/working_dir/PPC_MD/sub_list.txt');
+fid = fopen('~/scratch/working_dir/PPC_MD/sub_list.txt');
 sublist = textscan(fid,'%s');
 fclose(fid);
 sublist=sublist{1,1};
