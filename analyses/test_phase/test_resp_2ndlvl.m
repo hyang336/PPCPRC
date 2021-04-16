fddde0b0656f4647602700c1e0eda8c3a3345335
hyang336 <haozi078@gmail.com>
2021-04-15 22:08:37 -0400

    changed test phase 2nd lvl script to run each contrast separately

--- analyses/test_phase/test_resp_2ndlvl.m
+++ analyses/test_phase/test_resp_2ndlvl.m
@@ -3,7 +3,7 @@
 %running this
 
 
-function test_resp_2ndlvl(con_dir,output_dir,sublist)
+function test_resp_2ndlvl(con_dir,output_dir,sublist,contrast)
 
 %read in subject IDs
 fid=fopen(sublist,'r');
@@ -14,30 +14,6 @@ while ischar(tline)
     tline=fgetl(fid);
 end
 fclose(fid);
-
-
-if ~exist(strcat(output_dir,'/lifetime_main'),'dir')
-    mkdir (output_dir,'lifetime_main');
-end
-if ~exist(strcat(output_dir,'/recent_main'),'dir')
-    mkdir (output_dir,'recent_main');
-end
-if ~exist(strcat(output_dir,'/lifetime_pmod'),'dir')
-    mkdir (output_dir,'lifetime_pmod');
-end
-if ~exist(strcat(output_dir,'/recent_pmod'),'dir')
-    mkdir (output_dir,'recent_pmod');
-end
-% if ~exist(strcat(output_dir,'/featmain_lifetime'),'dir')
-%     mkdir (output_dir,'featmain_lifetime');
-% end
-% if ~exist(strcat(output_dir,'/featmain_recent'),'dir')
-%     mkdir (output_dir,'featmain_recent');
-% end
-% if ~exist(strcat(output_dir,'/featmain_overall'),'dir')
-%     mkdir (output_dir,'featmain_overall');
-% end
-
 
 %initil setup for SPM
 spm('defaults', 'FMRI');
@@ -65,64 +41,100 @@ spm_jobman('initcfg');
 % exposure.
 
 %%   
-%load job template
-test_resp_2ndlvl_job;
 
-% main effect of lifetime and recent exposure, one-sample t-tests 
-file_cell=cell(0,1);
-for i=1:length(SSID)
-    file_cell{i,1}=strcat(con_dir,'/sub-',SSID{i,1},'/temp/con_0001.nii');
-end
-matlabbatch{1}.spm.stats.factorial_design.dir = {strcat(output_dir,'/lifetime_main')};%specify
-matlabbatch{1}.spm.stats.factorial_design.des.t1.scans = file_cell;
-matlabbatch{2}.spm.stats.fmri_est.spmmat = {strcat(output_dir,'/lifetime_main/SPM.mat')};%estimate
-matlabbatch{3}.spm.stats.con.spmmat = {strcat(output_dir,'/lifetime_main/SPM.mat')};%contrast
-matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'lifetime_main';
-matlabbatch{3}.spm.stats.con.consess{1}.tcon.weights = 1;
-matlabbatch{4}.spm.stats.results.spmmat = {strcat(output_dir,'/lifetime_main/SPM.mat')};%threshold
-matlabbatch{4}.spm.stats.results.conspec(1).titlestr = 'lifetime_main';
 
-file_cell=cell(0,1);
-for i=1:length(SSID)
-    file_cell{i,1}=strcat(con_dir,'/sub-',SSID{i,1},'/temp/con_0002.nii');
-end
-matlabbatch{5}.spm.stats.factorial_design.dir = {strcat(output_dir,'/recent_main')};%specify
-matlabbatch{5}.spm.stats.factorial_design.des.t1.scans = file_cell;
-matlabbatch{6}.spm.stats.fmri_est.spmmat = {strcat(output_dir,'/recent_main/SPM.mat')};%estimate
-matlabbatch{7}.spm.stats.con.spmmat = {strcat(output_dir,'/recent_main/SPM.mat')};%contrast
-matlabbatch{7}.spm.stats.con.consess{1}.tcon.name = 'recent_main';
-matlabbatch{7}.spm.stats.con.consess{1}.tcon.weights = 1;
-matlabbatch{8}.spm.stats.results.spmmat = {strcat(output_dir,'/recent_main/SPM.mat')};%threshold
-matlabbatch{8}.spm.stats.results.conspec(1).titlestr = 'recent_main';
+switch contrast
+    
+    case 'lifetime_main'
+        %load job template
+        test_resp_2ndlvl_job_indi_singleT;
+        
+        if ~exist(strcat(output_dir,'/lifetime_main'),'dir')
+            mkdir (output_dir,'lifetime_main');
+        end
+        file_cell=cell(0,1);
+        for i=1:length(SSID)
+            file_cell{i,1}=strcat(con_dir,'/sub-',SSID{i,1},'/temp/con_0001.nii');
+        end
+        matlabbatch{1}.spm.stats.factorial_design.dir = {strcat(output_dir,'/lifetime_main')};%specify
+        matlabbatch{1}.spm.stats.factorial_design.des.t1.scans = file_cell;
+        matlabbatch{2}.spm.stats.fmri_est.spmmat = {strcat(output_dir,'/lifetime_main/SPM.mat')};%estimate
+        matlabbatch{3}.spm.stats.con.spmmat = {strcat(output_dir,'/lifetime_main/SPM.mat')};%contrast
+        matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'lifetime_main';
+        matlabbatch{3}.spm.stats.con.consess{1}.tcon.weights = 1;
+        matlabbatch{4}.spm.stats.results.spmmat = {strcat(output_dir,'/lifetime_main/SPM.mat')};%threshold
+        matlabbatch{4}.spm.stats.results.conspec(1).titlestr = 'lifetime_main';
+    case 'recent_main'
+        %load job template
+        test_resp_2ndlvl_job_indi_singleT;
 
-%paired t-test between modulated and unmodulated contrasts
-file_cell=cell(0,2);
-for i=1:length(SSID)
-    file_cell{i,1}=strcat(con_dir,'/sub-',SSID{i,1},'/temp/spmT_0003.nii');
-    file_cell{i,2}=strcat(con_dir,'/sub-',SSID{i,1},'/temp/spmT_0001.nii');
-    matlabbatch{9}.spm.stats.factorial_design.des.pt.pair(i).scans=file_cell(i,:)';
+        if ~exist(strcat(output_dir,'/recent_main'),'dir')
+            mkdir (output_dir,'recent_main');
+        end
+        file_cell=cell(0,1);
+        for i=1:length(SSID)
+            file_cell{i,1}=strcat(con_dir,'/sub-',SSID{i,1},'/temp/con_0002.nii');
+        end
+        matlabbatch{1}.spm.stats.factorial_design.dir = {strcat(output_dir,'/recent_main')};%specify
+        matlabbatch{1}.spm.stats.factorial_design.des.t1.scans = file_cell;
+        matlabbatch{2}.spm.stats.fmri_est.spmmat = {strcat(output_dir,'/recent_main/SPM.mat')};%estimate
+        matlabbatch{3}.spm.stats.con.spmmat = {strcat(output_dir,'/recent_main/SPM.mat')};%contrast
+        matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'recent_main';
+        matlabbatch{3}.spm.stats.con.consess{1}.tcon.weights = 1;
+        matlabbatch{4}.spm.stats.results.spmmat = {strcat(output_dir,'/recent_main/SPM.mat')};%threshold
+        matlabbatch{4}.spm.stats.results.conspec(1).titlestr = 'recent_main';
+    case 'lifetime_pmod'
+        %load job template
+        test_resp_2ndlvl_job_indi_pairedT;
+        
+        if ~exist(strcat(output_dir,'/lifetime_pmod'),'dir')
+            mkdir (output_dir,'lifetime_pmod');
+        end
+        file_cell=cell(0,2);
+        for i=1:length(SSID)
+            file_cell{i,1}=strcat(con_dir,'/sub-',SSID{i,1},'/temp/spmT_0003.nii');
+            file_cell{i,2}=strcat(con_dir,'/sub-',SSID{i,1},'/temp/spmT_0001.nii');
+            matlabbatch{1}.spm.stats.factorial_design.des.pt.pair(i).scans=file_cell(i,:)';
+        end
+        matlabbatch{1}.spm.stats.factorial_design.dir = {strcat(output_dir,'/lifetime_pmod')};%specify
+        matlabbatch{2}.spm.stats.fmri_est.spmmat = {strcat(output_dir,'/lifetime_pmod/SPM.mat')};%estimate
+        matlabbatch{3}.spm.stats.con.spmmat = {strcat(output_dir,'/lifetime_pmod/SPM.mat')};%contrast
+        matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'lifetime_pmod';
+        matlabbatch{3}.spm.stats.con.consess{1}.tcon.weights = [1,-1];
+        matlabbatch{4}.spm.stats.results.spmmat = {strcat(output_dir,'/lifetime_pmod/SPM.mat')};%threshold
+        matlabbatch{4}.spm.stats.results.conspec(1).titlestr = 'lifetime_pmod';
+    case 'recent_pmod'
+        %load job template
+        test_resp_2ndlvl_job_indi_pairedT;
+        
+        if ~exist(strcat(output_dir,'/recent_pmod'),'dir')
+            mkdir (output_dir,'recent_pmod');
+        end
+        file_cell=cell(0,2);
+        for i=1:length(SSID)
+            file_cell{i,1}=strcat(con_dir,'/sub-',SSID{i,1},'/temp/spmT_0004.nii');
+            file_cell{i,2}=strcat(con_dir,'/sub-',SSID{i,1},'/temp/spmT_0002.nii');
+            matlabbatch{1}.spm.stats.factorial_design.des.pt.pair(i).scans=file_cell(i,:)';
+        end
+        matlabbatch{1}.spm.stats.factorial_design.dir = {strcat(output_dir,'/recent_pmod')};%specify
+        matlabbatch{2}.spm.stats.fmri_est.spmmat = {strcat(output_dir,'/recent_pmod/SPM.mat')};%estimate
+        matlabbatch{3}.spm.stats.con.spmmat = {strcat(output_dir,'/recent_pmod/SPM.mat')};%contrast
+        matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = 'recent_pmod';
+        matlabbatch{3}.spm.stats.con.consess{1}.tcon.weights = [1,-1];
+        matlabbatch{4}.spm.stats.results.spmmat = {strcat(output_dir,'/recent_pmod/SPM.mat')};%threshold
+        matlabbatch{4}.spm.stats.results.conspec(1).titlestr = 'recent_pmod';
 end
-matlabbatch{9}.spm.stats.factorial_design.dir = {strcat(output_dir,'/lifetime_pmod')};%specify
-matlabbatch{10}.spm.stats.fmri_est.spmmat = {strcat(output_dir,'/lifetime_pmod/SPM.mat')};%estimate
-matlabbatch{11}.spm.stats.con.spmmat = {strcat(output_dir,'/lifetime_pmod/SPM.mat')};%contrast
-matlabbatch{11}.spm.stats.con.consess{1}.tcon.name = 'lifetime_pmod';
-matlabbatch{11}.spm.stats.con.consess{1}.tcon.weights = [1,-1];
-matlabbatch{12}.spm.stats.results.spmmat = {strcat(output_dir,'/lifetime_pmod/SPM.mat')};%threshold
-matlabbatch{12}.spm.stats.results.conspec(1).titlestr = 'lifetime_pmod';
 
-file_cell=cell(0,2);
-for i=1:length(SSID)
-    file_cell{i,1}=strcat(con_dir,'/sub-',SSID{i,1},'/temp/spmT_0004.nii');
-    file_cell{i,2}=strcat(con_dir,'/sub-',SSID{i,1},'/temp/spmT_0002.nii');
-    matlabbatch{13}.spm.stats.factorial_design.des.pt.pair(i).scans=file_cell(i,:)';
-end
-matlabbatch{13}.spm.stats.factorial_design.dir = {strcat(output_dir,'/recent_pmod')};%specify
-matlabbatch{14}.spm.stats.fmri_est.spmmat = {strcat(output_dir,'/recent_pmod/SPM.mat')};%estimate
-matlabbatch{15}.spm.stats.con.spmmat = {strcat(output_dir,'/recent_pmod/SPM.mat')};%contrast
-matlabbatch{15}.spm.stats.con.consess{1}.tcon.name = 'recent_pmod';
-matlabbatch{15}.spm.stats.con.consess{1}.tcon.weights = [1,-1];
-matlabbatch{16}.spm.stats.results.spmmat = {strcat(output_dir,'/recent_pmod/SPM.mat')};%threshold
-matlabbatch{16}.spm.stats.results.conspec(1).titlestr = 'recent_pmod';
+% if ~exist(strcat(output_dir,'/featmain_lifetime'),'dir')
+%     mkdir (output_dir,'featmain_lifetime');
+% end
+% if ~exist(strcat(output_dir,'/featmain_recent'),'dir')
+%     mkdir (output_dir,'featmain_recent');
+% end
+% if ~exist(strcat(output_dir,'/featmain_overall'),'dir')
+%     mkdir (output_dir,'featmain_overall');
+% end
+
 
 %skip the results part since it will get stuck. Will need to
 %find another way to threshold
