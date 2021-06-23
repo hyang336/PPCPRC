2d5df7bece1310f5df30c76bf50dc5e232239818
hyang336 <haozi078@gmail.com>
2021-06-22 21:56:41 -0400

    made conjunction script more general, now take con imgs from different directories

--- analyses/test_phase/conjunction_2ndlvl.m
+++ analyses/test_phase/conjunction_2ndlvl.m
@@ -1,4 +1,7 @@
-%% test phase conjunction analysis similar to (Duke et al., 2017)
+%% 2021-06-18 updated to be more general
+% now the contrast_directory and the contrast_list need to
+% have the same length, and are paired one-to-one
+%% test phase conjunction analysis similar to (Duke et al., 2017), but testing against global null
 % using the procedure described at https://www.jiscmail.ac.uk/cgi-bin/webadmin?A2=ind04&L=SPM&P=R187333
 %% for '/test_1stlvl_all3_softAROMA_bin/' contrast images are:
 % 0001: lifetime high>low main effect
@@ -20,7 +23,13 @@
 % 0003: dec-irr lifetime linear increase
 % 0004: dec-irr lifetime linear decrease
 
-function test_2ndlvl_conjunction(con_dir,output_dir,sublist,contrast_list,maskfile)
+%% for '/repetition_suppression_softAROMA_const-epoch/' contrast images are:
+%contrast 0001 pres_1>pres_2
+%contrast 0002 pres_1>baseline
+%contrast 0003 pres_2>baseline
+%contrast 0004 pres_1>avg(pres_789)
+%contrast 0005 pres_1 throught pres_9 decrease
+function conjunction_2ndlvl(con_dir_list,output_dir,sublist,contrast_list,maskfile)
 
 if ~exist(output_dir,'dir')
     mkdir (output_dir);
@@ -46,15 +55,29 @@ while ischar(tline)
 end
 fclose(fid);
 
+%read in contrast directory list
+fid=fopen(con_dir_list,'r');
+tline=fgetl(fid);
+con_dir=cell(0,1);
+while ischar(tline)
+    con_dir{end+1,1}=tline;
+    tline=fgetl(fid);
+end
+fclose(fid);
+
+%assert that the contrast directory list and the contrast
+%list are of the same length
+assert(length(contrasts)==length(con_dir));
+
 %initil setup for SPM
 spm('defaults', 'FMRI');
 spm_jobman('initcfg');
 
 %initialize job structure
-test_2ndlvl_conjunctive_job;
+conjunctive_2ndlvl_job;
 for i=1:length(SSID)%loop over subjects
     for j=1:length(contrasts) %loop over contrasts
-        confile=strcat(con_dir,'/sub-',SSID{i},'/temp/',contrasts{j},'.nii,1');
+        confile=strcat(con_dir{j},'/sub-',SSID{i},'/temp/',contrasts{j},'.nii,1');
         matlabbatch{1}.spm.stats.factorial_design.des.anova.icell(j).scans{i}=confile;
     end
 end
