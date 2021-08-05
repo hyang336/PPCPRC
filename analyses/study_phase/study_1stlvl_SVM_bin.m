15116955b5a10e604955078551d6425408ae6604
hyang336 <haozi078@gmail.com>
2021-08-04 20:32:36 -0400

    fix noresp in postscan ratings

--- analyses/study_phase/study_1stlvl_SVM_bin.m
+++ analyses/study_phase/study_1stlvl_SVM_bin.m
@@ -32,13 +32,18 @@ end
 [freq_high,~]=find(cellfun(@(x) mod(x,10),runevent(:,2))==6|cellfun(@(x) mod(x,10),runevent(:,2))==7|cellfun(@(x) mod(x,10),runevent(:,2))==8|cellfun(@(x) mod(x,10),runevent(:,2))==9);
 [freq_low,~]=find(cellfun(@(x) mod(x,10),runevent(:,2))==1|cellfun(@(x) mod(x,10),runevent(:,2))==2|cellfun(@(x) mod(x,10),runevent(:,2))==3|cellfun(@(x) mod(x,10),runevent(:,2))==4);
 %recode lifetime around subject mean
-submean=nanmean(str2num(cell2mat(runevent(:,13))));%calculate mean
-runevent(:,13)=num2cell(str2num(cell2mat(runevent(:,13))));%replace lifetime with num
+submean=nanmean(str2double(runevent(:,13)));%calculate mean
+runevent(:,13)=num2cell(str2double(runevent(:,13)));%replace lifetime with num
 [life_high,~]=find(cellfun(@(x) x>submean,runevent(:,13)));
 [life_low,~]=find(cellfun(@(x) x<submean,runevent(:,13)));
 
+%% sample trials to equalize high vs. low 10 times to guarantee every trial is at least included once following (Martin et al. 2013, 2016)
+% 1. First sample the larger class (a) n times without replacement until we have
+% less trials (x) than the smaller class (b), then sample
+% b-a from a-b, which guarantees that all trials are
+% included at least once. Then we just randomly sample
+% without replacement from a until we have 10 samples.
 
-%% sample trials to equalize high vs. low
  %to train on freq
  if length(freq_high)<=length(freq_low)
      freq_low_sample=datasample(freq_low,length(freq_high),'Replace',false);
