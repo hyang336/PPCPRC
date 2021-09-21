d8d0f561c9ae6b98e31876ef2da102f92b1f5604
hyang336 <haozi078@gmail.com>
2021-09-21 15:16:37 -0400

    added scripts for nonpara single_t test

---
+++ analyses/test_phase/SnPM_multisub_singleT_job.m
@@ -0,0 +1,20 @@
+%-----------------------------------------------------------------------
+% Job saved on 21-Sep-2021 14:14:40 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.tools.snpm.des.OneSampT.DesignName = 'MultiSub: One Sample T test on diffs/contrasts';
+matlabbatch{1}.spm.tools.snpm.des.OneSampT.DesignFile = 'snpm_bch_ui_OneSampT';
+matlabbatch{1}.spm.tools.snpm.des.OneSampT.dir = {'/scratch/hyang336/working_dir/PPC_MD/GLM_avgMask_4mmSmooth/'};
+matlabbatch{1}.spm.tools.snpm.des.OneSampT.P = {'C:\Users\haozi\Desktop\PhD\fMRI_PrC-PPC_data\testphase_2ndlvl\test_lifetime_recent_conjunction_both_inc\pTFCE_spmT_0002.nii,1'};
+matlabbatch{1}.spm.tools.snpm.des.OneSampT.cov = struct('c', {}, 'cname', {});
+matlabbatch{1}.spm.tools.snpm.des.OneSampT.nPerm = 5000;
+matlabbatch{1}.spm.tools.snpm.des.OneSampT.vFWHM = [0 0 0];
+matlabbatch{1}.spm.tools.snpm.des.OneSampT.bVolm = 1;
+matlabbatch{1}.spm.tools.snpm.des.OneSampT.ST.ST_none = 0;
+matlabbatch{1}.spm.tools.snpm.des.OneSampT.masking.tm.tm_none = 1;
+matlabbatch{1}.spm.tools.snpm.des.OneSampT.masking.im = 0;
+matlabbatch{1}.spm.tools.snpm.des.OneSampT.masking.em = {'/scratch/hyang336/working_dir/PPC_MD/masks/func-res_bin_30Ss_libmotion_testphase_MNI152NLin6_avgMask.nii,1'};
+matlabbatch{1}.spm.tools.snpm.des.OneSampT.globalc.g_omit = 1;
+matlabbatch{1}.spm.tools.snpm.des.OneSampT.globalm.gmsca.gmsca_no = 1;
+matlabbatch{1}.spm.tools.snpm.des.OneSampT.globalm.glonorm = 1;
---
+++ analyses/test_phase/SnPM_singleT.m
@@ -0,0 +1,36 @@
+%% use SnPM to correct for multiple comparison (nonparametrically)
+%% pass in the whole-brain mask for whole brain analysis, PrC mask for SVC
+function SnPM_singleT(lvl1_dir,contrast,sublist,output_dir,mask)
+    if ~exist(output_dir,'dir')
+    mkdir (output_dir);
+end
+
+%read in subject IDs
+fid=fopen(sublist,'r');
+tline=fgetl(fid);
+SSID=cell(0,1);
+while ischar(tline)
+    SSID{end+1,1}=tline;
+    tline=fgetl(fid);
+end
+fclose(fid);
+
+%initil setup for SPM
+spm('defaults', 'FMRI');
+spm_jobman('initcfg');
+
+%initialize job template
+SnPM_multisub_singleT_job
+
+%loop over subjects to load contrast images (one per subject)
+for i=1:length(SSID)%loop over subjects
+        confile=strcat(lvl1_dir{j},'/sub-',SSID{i},'/temp/',contrast,'.nii,1');
+        matlabbatch{1}.spm.tools.snpm.des.OneSampT.P{i}=confile;
+end
+matlabbatch{1}.spm.tools.snpm.des.OneSampT.vFWHM=[4,4,4];%variance smoothing equals the smoothing extent of the data
+matlabbatch{1}.spm.tools.snpm.des.OneSampT.masking.em = {mask};
+matlabbatch{1}.spm.tools.snpm.des.OneSampT.dir = {output_dir};
+
+spm_jobman('run',matlabbatch);
+
+end
