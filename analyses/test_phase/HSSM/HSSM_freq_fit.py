eee1709300d2bd6efff1ba84558b52b9f867f95a
hyang336 <haozi078@gmail.com>
2024-06-10 22:41:24 -0400

    Update HSSM_freq_fit.py

--- analyses/test_phase/HSSM/HSSM_freq_fit.py
+++ analyses/test_phase/HSSM/HSSM_freq_fit.py
@@ -17,214 +17,187 @@ if __name__ == '__main__':
 
     #parse arguments
     parser = argparse.ArgumentParser(description='Simulate data and fit HSSM model')
+    parser.add_argument('--signal', type=str, help='which familiarity signal to model',default='recent')
     parser.add_argument('--model', type=str, help='which model to run')
     parser.add_argument('--outdir', type=str, help='outpu directory to save results',default='/scratch/hyang336/working_dir/HDDM_HSSM/ROIs/')
-    parser.add_argument('--InData', type=str, help='CSV containing behavioral data and regressors')
+    parser.add_argument('--bin', type=str, help='which two responses to bin together',default='12')
     args = parser.parse_args()
 
-    model=args.model
+    signalname=args.signal
+    modelname=args.model
     outdir=args.outdir
-    csv_file=args.InData
-    #--------------------------------------We can try several generative model--------------------------------###
-    #load in CSV
-    InData=pd.read_csv(csv_file)
+    bin_ver=args.bin
+    
+###################################################################    # load the data###################################################################
+    if signalname == 'recent':
+        # load the csv
+        fam_data = pd.read_csv('/scratch/hyang336/working_dir/HDDM_HSSM/HSSM_freq_data.csv')
+    elif signalname == 'lifetime':
+        # load the csv
+        fam_data = pd.read_csv('/scratch/hyang336/working_dir/HDDM_HSSM/HSSM_life_data.csv')
+########################################################################################################################################################   
 
+#####################################################   # subset the dataframe based on binning version###################################################################
+    match bin_ver:
+        case '12':
+            # subset the dataframe by selecting relevant columns
+            sim_data = fam_data[['subj_idx','rt','bin12_rating']]
+            # rename the rating column to response
+            sim_data = sim_data.rename(columns={'bin12_rating':'response'})
+            
+        case '23':
+            # subset the dataframe by selecting relevant columns
+            sim_data = fam_data[['subj_idx','rt','bin23_rating']]
+            # rename the rating column to response
+            sim_data = sim_data.rename(columns={'bin23_rating':'response'})
 
-    # Generate subject-level parameters
-    for i in range(n_subjects):
-        # set the seed for each subject deterministically so all models are based on the same data
-        np.random.seed(i)
-        # generate neural data
-        simneural=np.random.uniform(0, 1, size=n_trials)
-        # generate v0, v1, v2, v3
-        v0=np.exp(np.random.normal(intercept0, param_sv) + np.random.normal(a0, param_sv)*np.log(simneural) + np.random.normal(b0, param_sv)*np.log(1-simneural))
-        v1=np.exp(np.random.normal(intercept1, param_sv) + np.random.normal(a1, param_sv)*np.log(simneural) + np.random.normal(b1, param_sv)*np.log(1-simneural))
-        v2=np.exp(np.random.normal(intercept2, param_sv) + np.random.normal(a2, param_sv)*np.log(simneural) + np.random.normal(b2, param_sv)*np.log(1-simneural))
-        v3=np.exp(np.random.normal(intercept3, param_sv) + np.random.normal(a3, param_sv)*np.log(simneural) + np.random.normal(b3, param_sv)*np.log(1-simneural))
+        case '34':
+            # subset the dataframe by selecting relevant columns
+            sim_data = fam_data[['subj_idx','rt','bin34_rating']]
+            # rename the rating column to response
+            sim_data = sim_data.rename(columns={'bin34_rating':'response'})
+
+        case '45':
+            # subset the dataframe by selecting relevant columns
+            sim_data = fam_data[['subj_idx','rt','bin45_rating']]
+            # rename the rating column to response
+            sim_data = sim_data.rename(columns={'bin45_rating':'response'})
+########################################################################################################################################################
+    
+################    ## Define model, null model is a special case since it doesn't have regressor###################################################################
+    if modelname == 'null':
+        # format data to be fed into the model, NOTE that "rt" and "response" are reserved keywords
+        data = {
+            'rt':sim_data['rt'],
+            'response':sim_data['response'],
+            'subj_idx':sim_data['subj_idx']
+        }
         
-        ###IMPORTANT: for interpretable param rec test, make sure generate params within training bounds of LAN###
-        # only keep entries in subject_data that are in bounds
-        v0_inb= np.where(np.logical_and(v0>= 0,v0<= 2.5))
-        v1_inb= np.where(np.logical_and(v1>= 0,v1<= 2.5))
-        v01_inb=np.intersect1d(v0_inb[0],v1_inb[0])
-
-        v2_inb= np.where(np.logical_and(v2>= 0,v2<= 2.5))
-        v3_inb= np.where(np.logical_and(v3>= 0,v3<= 2.5))
-        v23_inb=np.intersect1d(v2_inb[0],v3_inb[0])
-
-        v0123_inb=np.intersect1d(v01_inb,v23_inb)#indices of elements that are in bound for all 4 arrays
-
-        #only keep inbound elements
-        simneural=simneural[v0123_inb]
-        v0=v0[v0123_inb]
-        v1=v1[v0123_inb]
-        v2=v2[v0123_inb]
-        v3=v3[v0123_inb]
-        
-        # save to subject_params
-        subject_params["v0"]=np.append(subject_params["v0"],v0)
-        subject_params["v1"]=np.append(subject_params["v1"],v1)
-        subject_params["v2"]=np.append(subject_params["v2"],v2)
-        subject_params["v3"]=np.append(subject_params["v3"],v3)
-        subject_params["simneural"]=np.append(subject_params["simneural"],simneural)
-        subject_params["subID"]=np.append(subject_params["subID"],np.repeat(i,len(simneural)))
-
-        # simulate RT and choices
-        true_values = np.column_stack(
-        [v0,v1,v2,v3, np.repeat([[2.0, 0.0, 1e-3,0.0]], axis=0, repeats=len(simneural))]
-        )
-        # Get mode simulations
-        race4nba_v = simulator.simulator(true_values, model="race_no_bias_angle_4", n_samples=1)
-
-        # Random regressor as control
-        rand_x = np.random.uniform(0, 1, size=len(simneural))
-        sim_data.append(
-            pd.DataFrame(
+        # define the model
+        model= hssm.HSSM(
+            data=data,
+            model='race_no_bias_angle_4',
+            a=2.0,
+            z=0.0,
+            include=[
                 {
-                    "rt": race4nba_v["rts"].flatten(),
-                    "response": race4nba_v["choices"].flatten(),
-                    "x": np.log(simneural),
-                    "y": np.log(1-simneural),
-                    "rand_x": np.log(rand_x),
-                    "rand_y": np.log(1-rand_x),
-                    "subID": i
+                    "name": "v0",
+                    "prior":{"name": "Uniform", "lower": -1, "upper": 3},
+                    "formula": "v0 ~ 1 + (1|subj_idx)",
+                    "link": "log",
+                },
+                {
+                    "name": "v1",
+                    "prior":{"name": "Uniform", "lower": -1, "upper": 3},
+                    "formula": "v1 ~ 1 + (1|subj_idx)",
+                    "link": "log",
+                },
+                {
+                    "name": "v2",
+                    "prior":{"name": "Uniform", "lower": -1, "upper": 3},
+                    "formula": "v2 ~ 1 + (1|subj_idx)",
+                    "link": "log",
+                },
+                {
+                    "name": "v3",
+                    "prior":{"name": "Uniform", "lower": -1, "upper": 3},
+                    "formula": "v3 ~ 1 + (1|subj_idx)",
+                    "link": "log",
                 }
-            )
+            ],
         )
+    else:
+        match modelname: #select the regressor based on model name
+            case 'rand':# control model with regression on randomly generated data
+                # extract another column from the fam_data and add to the sim_data based on modelname
+                sim_data['x'] = fam_data['random_z']
+            case 'hippo_pos':
+                # extract another column from the fam_data and add to the sim_data based on modelname
+                sim_data['x'] = fam_data['hippo_z_pos']
+            case 'hippo_neg':
+                # extract another column from the fam_data and add to the sim_data based on modelname
+                sim_data['x'] = fam_data['hippo_z_neg']
+            case 'PrC_neg':
+                # extract another column from the fam_data and add to the sim_data based on modelname
+                sim_data['x'] = fam_data['PrC_z']
+            case 'mPFC_pos':
+                # extract another column from the fam_data and add to the sim_data based on modelname
+                sim_data['x'] = fam_data['mPFC_z']
+            case 'mPPC_pos':
+                # extract another column from the fam_data and add to the sim_data based on modelname
+                sim_data['x'] = fam_data['mPPC_z']
+            case 'lAnG_pos':
+                # this model is only defined for lifetime familiarity signal, throw an error if the wrong signal is passed
+                if signalname != 'lifetime':
+                    raise ValueError('This model is only defined for lifetime familiarity signal')
+                # extract another column from the fam_data and add to the sim_data based on modelname
+                sim_data['x'] = fam_data['lAnG_z']
+            case 'lSFG_pos':
+                # this model is only defined for lifetime familiarity signal, throw an error if the wrong signal is passed
+                if signalname != 'lifetime':
+                    raise ValueError('This model is only defined for lifetime familiarity signal')
+                # extract another column from the fam_data and add to the sim_data based on modelname
+                sim_data['x'] = fam_data['lSFG_z']
 
-    #make a single dataframe of subject-wise simulated data
-    sim_data_concat=pd.concat(sim_data)
+        # rescale the regressor to be between 0 and 1 since that is what teh beta distribution is defined on
+        sim_data['x'] = (sim_data['x'] - np.min(sim_data['x']))/(np.max(sim_data['x']) - np.min(sim_data['x']))
+        # add the other regressor
+        sim_data['y'] = 1-sim_data['x']
 
-    # Define models
-    match model:
-        case 'true':
-            # True model
-            model_race4nba_v_true = hssm.HSSM(
-                data=sim_data_concat,
-                model='race_no_bias_angle_4',
-                a=2.0,
-                z=0.0,
-                include=[
-                    {
-                        "name": "v0",
-                        "prior":{"name": "Uniform", "lower": -1, "upper": 3},
-                        "formula": "v0 ~ 1 + x + y",
-                        "link": "log",
-                    },
-                    {
-                        "name": "v1",
-                        "prior":{"name": "Uniform", "lower": -1, "upper": 3},
-                        "formula": "v1 ~ 1 + x + y",
-                        "link": "log",
-                    },
-                    {
-                        "name": "v2",
-                        "prior":{"name": "Uniform", "lower": -1, "upper": 3},
-                        "formula": "v2 ~ 1 + x + y",
-                        "link": "log",
-                    },
-                    {
-                        "name": "v3",
-                        "prior":{"name": "Uniform", "lower": -1, "upper": 3},
-                        "formula": "v3 ~ 1 + x + y",
-                        "link": "log",
-                    }
-                ],
-            )
-            #sample from the model
-            infer_data_race4nba_v_true = model_race4nba_v_true.sample(step=pm.Slice(model=model_race4nba_v_true.pymc_model), sampler="mcmc", chains=4, cores=4, draws=5000, tune=10000,idata_kwargs = {'log_likelihood': True})
-            # compute WAIC
-            az.waic(infer_data_race4nba_v_true)
-            #save trace
-            az.to_netcdf(infer_data_race4nba_v_true,outdir+'sample_5000_10000_trace_ParamInbound_Fixed_az_SliceSampler_true.nc4')
-            #save trace plot
-            az.plot_trace(
-                infer_data_race4nba_v_true,
-                var_names="~log_likelihood",  # we exclude the log_likelihood traces here
-            )
-            plt.savefig(outdir+'posterior_diagnostic_5000_10000_trace_ParamInbound_Fixed_az_SliceSampler_true.png')
-            #save summary
-            res_sum_true=az.summary(model_race4nba_v_true.traces)
-            res_sum_true.to_csv(outdir+'summary_5000_10000_trace_ParamInbound_Fixed_az_SliceSampler_true.csv')
-        
-        case 'null':
-            # model with no relationship between v and neural data
-            model_race4nba_v_null = hssm.HSSM(
-                data=sim_data_concat,
-                model='race_no_bias_angle_4',
-                a=2.0,
-                z=0.0,
-                include=[
-                    {
-                        "name": "v0",
-                        "prior":{"name": "Uniform", "lower": -1, "upper": 3},
-                    },
-                    {
-                        "name": "v1",
-                        "prior":{"name": "Uniform", "lower": -1, "upper": 3},
-                    },
-                    {
-                        "name": "v2",
-                        "prior":{"name": "Uniform", "lower": -1, "upper": 3},
-                    },
-                    {
-                        "name": "v3",
-                        "prior":{"name": "Uniform", "lower": -1, "upper": 3},
-                    }
-                ],
-            )
-            infer_data_race4nba_v_null = model_race4nba_v_null.sample(step=pm.Slice(model=model_race4nba_v_null.pymc_model), sampler="mcmc", chains=4, cores=4, draws=5000, tune=10000,idata_kwargs = {'log_likelihood': True})
-            az.waic(infer_data_race4nba_v_null)
-            az.to_netcdf(infer_data_race4nba_v_null,outdir+'sample_5000_10000_trace_ParamInbound_Fixed_az_SliceSampler_null.nc4')
-            az.plot_trace(
-                infer_data_race4nba_v_null,
-                var_names="~log_likelihood",  # we exclude the log_likelihood traces here
-            )
-            plt.savefig(outdir+'posterior_diagnostic_5000_10000_trace_ParamInbound_Fixed_az_SliceSampler_null.png')
-            res_sum_null=az.summary(model_race4nba_v_null.traces)
-            res_sum_null.to_csv(outdir+'summary_5000_10000_trace_ParamInbound_Fixed_az_SliceSampler_null.csv')
+        # format data to be fed into the model, NOTE that "rt" and "response" are reserved keywords
+        data = {
+            'rt':sim_data['rt'],
+            'response':sim_data['response'],
+            'x':sim_data['x'],
+            'y':sim_data['y'],
+            'subj_idx':sim_data['subj_idx']
+        }
 
-        case 'rand':
-            # model with regression on random vectors (i.e. fake neural data that has the same distribution but was not involved in generating the parameters)
-            model_race4nba_v_rand = hssm.HSSM(
-                data=sim_data_concat,
-                model='race_no_bias_angle_4',
-                a=2.0,
-                z=0.0,
-                include=[
-                    {
-                        "name": "v0",
-                        "prior":{"name": "Uniform", "lower": -1, "upper": 3},
-                        "formula": "v0 ~ 1 + rand_x + rand_y",
-                        "link": "log",
-                    },
-                    {
-                        "name": "v1",
-                        "prior":{"name": "Uniform", "lower": -1, "upper": 3},
-                        "formula": "v1 ~ 1 + rand_x + rand_y",
-                        "link": "log",
-                    },
-                    {
-                        "name": "v2",
-                        "prior":{"name": "Uniform", "lower": -1, "upper": 3},
-                        "formula": "v2 ~ 1 + rand_x + rand_y",
-                        "link": "log",
-                    },
-                    {
-                        "name": "v3",
-                        "prior":{"name": "Uniform", "lower": -1, "upper": 3},
-                        "formula": "v3 ~ 1 + rand_x + rand_y",
-                        "link": "log",
-                    }
-                ],
-            )
-            infer_data_race4nba_v_rand = model_race4nba_v_rand.sample(step=pm.Slice(model=model_race4nba_v_rand.pymc_model), sampler="mcmc", chains=4, cores=4, draws=5000, tune=10000,idata_kwargs = {'log_likelihood': True})
-            az.waic(infer_data_race4nba_v_rand)
-            az.to_netcdf(infer_data_race4nba_v_rand,outdir+'sample_5000_10000_trace_ParamInbound_Fixed_az_SliceSampler_rand.nc4')
-            az.plot_trace(
-                infer_data_race4nba_v_rand,
-                var_names="~log_likelihood",  # we exclude the log_likelihood traces here
-            )
-            plt.savefig(outdir+'posterior_diagnostic_5000_10000_trace_ParamInbound_Fixed_az_SliceSampler_rand.png')
-            res_sum_rand=az.summary(model_race4nba_v_rand.traces)
-            res_sum_rand.to_csv(outdir+'summary_5000_10000_trace_ParamInbound_Fixed_az_SliceSampler_rand.csv')
+        # define the model
+        model= hssm.HSSM(
+            data=data,
+            model='race_no_bias_angle_4',
+            a=2.0,
+            z=0.0,
+            include=[
+                {
+                    "name": "v0",
+                    "prior":{"name": "Uniform", "lower": -1, "upper": 3},
+                    "formula": "v0 ~ 1 + (x|subj_idx) + (y|subj_idx)",
+                    "link": "log",
+                },
+                {
+                    "name": "v1",
+                    "prior":{"name": "Uniform", "lower": -1, "upper": 3},
+                    "formula": "v1 ~ 1 + (x|subj_idx) + (y|subj_idx)",
+                    "link": "log",
+                },
+                {
+                    "name": "v2",
+                    "prior":{"name": "Uniform", "lower": -1, "upper": 3},
+                    "formula": "v2 ~ 1 + (x|subj_idx) + (y|subj_idx)",
+                    "link": "log",
+                },
+                {
+                    "name": "v3",
+                    "prior":{"name": "Uniform", "lower": -1, "upper": 3},
+                    "formula": "v3 ~ 1 + (x|subj_idx) + (y|subj_idx)",
+                    "link": "log",
+                }
+            ],
+        )
+########################################################################################################################################################
+
+    #sample from the model and save the results
+    infer_data_race4nba_v = model.sample(step=pm.Slice(model=model.pymc_model), sampler="mcmc", chains=4, cores=4, draws=5000, tune=10000,idata_kwargs = {'log_likelihood': True})
+    #save trace
+    az.to_netcdf(infer_data_race4nba_v,outdir +'sample_5000_10000_trace_Fixed_az_' + signalname + '_' + modelname + '_bin' + bin_ver + '.nc4')
+    #save trace plot
+    az.plot_trace(
+        infer_data_race4nba_v,
+        var_names="~log_likelihood",  # we exclude the log_likelihood traces here
+    )
+    plt.savefig(outdir+'posterior_diagnostic_5000_10000_trace_Fixed_az_' + signalname + '_' + modelname + '_bin' + bin_ver + '.png')
+    #save summary
+    res_sum=az.summary(model.traces)
+    res_sum.to_csv(outdir+'summary_5000_10000_trace_Fixed_az_' + signalname + '_' + modelname + '_bin' + bin_ver + '.csv')
