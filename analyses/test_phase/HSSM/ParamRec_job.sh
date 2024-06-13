8820df028fa458353c8ca71460a475c042e68145
hyang336 <haozi078@gmail.com>
2024-06-12 22:43:18 -0400

    Update ParamRec_job.sh

--- analyses/test_phase/HSSM/ParamRec_job.sh
+++ analyses/test_phase/HSSM/ParamRec_job.sh
@@ -3,7 +3,7 @@
 #SBATCH --account=ctb-akhanf
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
-#SBATCH --gpus-per-node=1
+#SBATCH --gpus-per-node=4
 #SBATCH --mem=32G
 #SBATCH --job-name=race4nb_ParRec
 #SBATCH --output=/home/hyang336/jobs/race4nb_ParRec%j.out
