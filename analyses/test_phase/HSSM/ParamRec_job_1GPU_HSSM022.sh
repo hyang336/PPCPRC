0ec41948988fd2be3635e685e10cbd6738f376a3
hyang336 <haozi078@gmail.com>
2024-06-15 19:01:47 -0400

    Create ParamRec_job_1GPU_HSSM022.sh
    attempted installing HSSM 0.2.2 since it contains a bug fix on convergence issue

---
+++ analyses/test_phase/HSSM/ParamRec_job_1GPU_HSSM022.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --time=48:00:00
+#SBATCH --account=ctb-akhanf
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=3
+#SBATCH --gres=gpu:v100:1
+#SBATCH --mem=64G
+#SBATCH --job-name=race4nb_ParRec
+#SBATCH --output=/home/hyang336/jobs/race4nb_ParRec%j.out
+
+#set up environment
+module load gcc cuda cudnn python/3.11
+virtualenv --no-download $SLURM_TMPDIR/ENV 
+source $SLURM_TMPDIR/ENV/bin/activate
+pip install --no-index --upgrade pip
+pip install --no-index -r /home/hyang336/PPCPRC/analyses/test_phase/HSSM/hssm-0.2.2-reqs.txt
+
+PYTENSOR_FLAGS='blas__ldflags=-lflexiblas -lgfortran' python /home/hyang336/PPCPRC/analyses/test_phase/HSSM/Simulations.py --model $1 --burnin $2
