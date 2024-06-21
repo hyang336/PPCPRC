4aadeb66117d98c76f2a4291f84260475f92d090
hyang336 <haozi078@gmail.com>
2024-06-21 01:37:37 -0400

    added parameter recovery script for ddm to diagnose convergence issue

---
+++ analyses/test_phase/HSSM/ParamRec_binary_job_1GPU_HSSM022.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --time=4:00:00
+#SBATCH --account=ctb-akhanf
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=3
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64G
+#SBATCH --job-name=race4nb_ParRec
+#SBATCH --output=/home/hyang336/jobs/race4nb_ParRec%j.out
+
+#set up environment
+module load gcc cuda cudnn python/3.11
+source ~/HSSM022_tempENV/bin/activate
+
+PYTENSOR_FLAGS='blas__ldflags=-lflexiblas -lgfortran' python /home/hyang336/PPCPRC/analyses/test_phase/HSSM/Simulations_binary022.py --model $1 --outdir '/scratch/hyang336/working_dir/HDDM_HSSM/simulations022/'
