9ea0f9155eb96c78b24e42563f422ca1dc6926d9
hyang336 <haozi078@gmail.com>
2024-06-21 20:23:37 -0400

    Update ParamRec_binary_job_1GPU_HSSM022.sh

--- analyses/test_phase/HSSM/ParamRec_binary_job_1GPU_HSSM022.sh
+++ analyses/test_phase/HSSM/ParamRec_binary_job_1GPU_HSSM022.sh
@@ -15,6 +15,8 @@ source ~/HSSM022_tempENV/bin/activate
 # whether to run the simple simulations (only v depends on regressor) or the full simulations
 if [ $1 == "simple" ]; then
     PYTENSOR_FLAGS='blas__ldflags=-lflexiblas -lgfortran' python /home/hyang336/PPCPRC/analyses/test_phase/HSSM/Simulations_binary022_simple.py --model $2 --outdir '/scratch/hyang336/working_dir/HDDM_HSSM/simulations022/'
+elif [ $1 == "frankmj" ]; then
+    PYTENSOR_FLAGS='blas__ldflags=-lflexiblas -lgfortran' python /home/hyang336/PPCPRC/analyses/test_phase/HSSM/Simulations_binary022_frankmj.py --outdir '/scratch/hyang336/working_dir/HDDM_HSSM/simulations022/'
 else
     PYTENSOR_FLAGS='blas__ldflags=-lflexiblas -lgfortran' python /home/hyang336/PPCPRC/analyses/test_phase/HSSM/Simulations_binary022.py --model $2 --outdir '/scratch/hyang336/working_dir/HDDM_HSSM/simulations022/'
 fi
